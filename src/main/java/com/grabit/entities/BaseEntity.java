b7ead7fcd947aa8f83715baa2a28dab6d60ba23d
IBruteDude <kamarf505@gmail.com>
2025-02-18 10:32:33 +0200

    fix(base): change primary key GenerationType to UUID

--- src/main/java/com/grabit/entities/BaseEntity.java
+++ src/main/java/com/grabit/entities/BaseEntity.java
@@ -22,7 +22,7 @@ import lombok.experimental.SuperBuilder;
 public class BaseEntity implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.UUID)
 	protected UUID id;
 
 	@CreatedDate
