a6e043b28201ce7127779117ed50af5cca040b01
IBruteDude <kamarf505@gmail.com>
2025-02-28 15:08:29 +0200

    fix(error): fixed the basic exception handler to be safer, log error, return localized ApiResponse

--- src/main/java/com/grabit/error/GlobalExceptionHandler.java
+++ src/main/java/com/grabit/error/GlobalExceptionHandler.java
@@ -1,16 +1,32 @@
 package com.grabit.error;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.Locale;
 
-@ControllerAdvice
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
-	ResponseEntity<?> handle(Exception e) {
-		System.out.printf("Handled exception %s", e.toString());
-		return ResponseEntity.badRequest().body(e);
-	}
+	@Autowired
+	private MessageSource messageSource;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<?> handleUnexpectedException(Exception ex, HttpServletRequest request) {
+        log.error("Unexpected error: ", ex);
+        Locale locale = request.getLocale();
+        String localizedMessage = messageSource.getMessage("error.unexpected", null, locale);
+
+        return ApiResponse.error(localizedMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
