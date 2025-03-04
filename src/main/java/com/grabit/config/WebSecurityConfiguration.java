bc4e4e36c854f1f24f3e40b3a6fae9c70b3dcefc
IBruteDude <kamarf505@gmail.com>
2025-03-04 17:22:09 +0200

    feat(security): added oauth2 login option & moved default user data to enviroment variables

--- src/main/java/com/grabit/config/WebSecurityConfiguration.java
+++ src/main/java/com/grabit/config/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.grabit.config;
 
-import java.util.UUID;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,80 +15,76 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
-
 import com.grabit.services.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
+	@Value("${DEFAULT_SECURITY_USERNAME}")
+	private String DEFAULT_SECURITY_USERNAME;
+
+	@Value("${DEFAULT_SECURITY_PASSWORD}")
+	private String DEFAULT_SECURITY_PASSWORD;
+
+
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http, DigestAuthenticationFilter digestAuthenticationFilter) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-				.headers(Customizer.withDefaults())
-				.formLogin(form -> form
-						.loginPage("/login")
-						.defaultSuccessUrl("/home")
-						.permitAll())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/login/**").permitAll()
+						.requestMatchers("/oauth2/**").permitAll()
+						.anyRequest().authenticated())
+				// .formLogin(form -> form
+				// 		.loginPage("/login")
+				// 		.defaultSuccessUrl("/home")
+				// 		.permitAll())
 				// .httpBasic(Customizer.withDefaults())
+				.oauth2Login(Customizer.withDefaults())
+				.oauth2Client(Customizer.withDefaults())
+				// .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+				// .csrf(csrf -> csrf.disable())
+				// .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				// .requiresChannel(channel -> channel.anyRequest().requiresSecure())
-				// .exceptionHandling(e -> e.authenticationEntryPoint(digestAuthenticationEntryPoint()))
-				// .addFilter(digestAuthenticationFilter)
+				// .headers(Customizer.withDefaults())
+				// .exceptionHandling((exceptions) -> exceptions
+				// 		.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+				// 		.accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
 				.build();
 	}
 
-
 	@Bean
 	@DependsOn("liquibase")
-    @Profile("prod")
+	@Profile("prod")
 	UserDetailsService productionUserDetailsService(UserService userService) {
 		return userDetailsService(userService);
 	}
 
-
 	@Bean
 	@DependsOnDatabaseInitialization
 	@DependsOn("entityManagerFactory")
-    @Profile("test")
+	@Profile("test")
 	@Primary
 	UserDetailsService testingUserDetailsService(UserService userService) {
 		return userDetailsService(userService);
 	}
 
-
 	UserDetailsService userDetailsService(UserService manager) {
-		var admin = User
-				.withUsername("admin")
-				.password("{noop}admin")
-				.roles( "GUEST", "CUSTOMER", "SUPPLIER", "SUPPORT_AGENT", "ANALYST", "ADMIN")
+		var user = User.withUsername(DEFAULT_SECURITY_USERNAME)
+				// .password(DEFAULT_SECURITY_USERNAME)
+				// .passwordEncoder(passwordEncoder()::encode)
+				.password(passwordEncoder().encode(DEFAULT_SECURITY_PASSWORD))
+				.roles("GUEST", "CUSTOMER", "SUPPLIER", "SUPPORT_AGENT", "ANALYST", "ADMIN")
 				.build();
 
-		if (!manager.userExists("admin"))
-			manager.createUser(admin);
+		if (!manager.userExists(DEFAULT_SECURITY_USERNAME))
+			manager.createUser(user);
 		return manager;
 	}
 
 	@Bean
-	DigestAuthenticationEntryPoint digestAuthenticationEntryPoint() {
-		DigestAuthenticationEntryPoint entrypoint = new DigestAuthenticationEntryPoint();
-		entrypoint.setRealmName("GrabIt Realm");
-		entrypoint.setKey(UUID.randomUUID().toString());
-		return entrypoint;
-	}
-
-	@Bean
-	DigestAuthenticationFilter digestAuthenticationFilter(UserDetailsService userDetailsService) {
-		DigestAuthenticationFilter filter = new DigestAuthenticationFilter();
-		filter.setUserDetailsService(userDetailsService);
-		filter.setAuthenticationEntryPoint(digestAuthenticationEntryPoint());
-		return filter;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 }
